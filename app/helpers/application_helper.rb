c920da18af30ee9f42f430228ecc934cbe7b41db
Menelik Tucker <meneliktucker@gmail.com>
2014-03-05 21:36:41 -0500

    Add header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def full_title(pages_title)
+		base_title = "SESA"
+		if pages_title.empty?
+			base_title
+		else
+			"#{base_title} | #{pages_title}"
+		end
+	end
+
 end
