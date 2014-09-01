b84b901f0a85696f052b64df69da6f1dfb5c9cc6
Menelik Tucker <meneliktucker@gmail.com>
2014-09-01 15:28:45 -0400

    fixing bug part 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -253,7 +253,6 @@ function start(){
 
 
 	// PAGE JUMPING JAVASCRIPT
-
 	$('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
         || location.hostname == this.hostname) {
