60faeec0657ef0be68f1bd5bd28d3fba0b1db40a
Craig <craigbryan.csb@gmail.com>
2015-01-22 23:47:23 -0500

    Removed a bunch of extraneous styling javascript.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,59 +53,30 @@ function start(){
 	//HEADER
 	if(window.innerWidth < 1025){
 			$("header .row").css({'opacity' : '0'});
-			$("header").css({'background-color' : 'initial'});
-			$("header").css({'position' : 'absolute'});
-			$("header .row").css({'display' : 'none'});
-			$(".hamburger").css({'position' : 'fixed'});
-			$(".hamburger").css({ 'display' : 'block' });
-			$("header").css({'height' : '0px'});
-			$('.join').css({'padding-top' : '0' });
 
-		$(".hamburger").click( function(){ 
-			if(oddClick){
+		$(".hamburger").click( function() { 
+			if(oddClick) {
 				$(".hamburger path:nth-child(1)").css({"transform" : "matrix(0.7071, 0.7071, -0.7071, 0.7071, 10, -7)"});
 				$(".hamburger path:nth-child(2)").css({"opacity" : "0"});
 				$(".hamburger path:nth-child(3)").css({"transform" : "matrix(0.7071, -0.7071, 0.7071, 0.7071, -10, 8)"});
-				$("header").css({'background-color' : '#eee'});
-				$("header").css({'position' : 'fixed'});
-				$("header .row").css({'display' : 'block'});
-				$("header").css({'height' : '490px'});
+				$("header").addClass("displayed");
 				$("header .row").delay(100).animate({ 'opacity' : '1' }, 250);
-			}else{
-				$("header").css({'height' : '0px'});
+
+			} else {
 				$(".hamburger path:nth-child(1)").css({"transform" : "matrix(1, 0, 0, 1, 0, 0)"});
 				$(".hamburger path:nth-child(2)").css({"opacity" : "1"});
 				$(".hamburger path:nth-child(3)").css({"transform" : "matrix(1, 0, 0, 1, 0, 0)"});
-				$("header").css({'position' : 'fixed'});
 				$("header .row").animate({ 'opacity' : '0' }, 50);
-				$("header .row").css({'display' : 'none'});
-				$("header").css({'background-color' : 'initial'});
+				$("header").removeClass("displayed");
 			}
 			oddClick = !oddClick;
 			return false;
 		 });
-	}else{
-		$('.join').css({'padding-top' : '70px'});
-		$(".hamburger").css({ 'display' : 'none' });
-		$("header").css({'height' : '70px'});
-		$("header").css({'position' : 'fixed'});
-		$("header").css({'background-color' : '#eee'});
+	} else {
 		$("header .row").css({'opacity' : '1'});
-		$("header .row").css({'display' : 'block'});
-	}
-
-
-	//FOOTER
-	$("footer").css({ 'height' : ($("footer > h1").height() + $("footer > .row1").height() + $("footer > .row2" + 20)) });
-	
+	}	
 
 	//JOIN PAGE
-	if($(".form .row").width() < 480){
-		$(".form .row").css({ 'padding' : '50px 5px 25px'});
-	}else{
-		$(".form .row").css({ 'padding' : '50px'});
-	}
-
 	$("span#close_successful_notice").click( function(){ 
 		$("#successful_notice").css({ "display" : "none" });
 		return false;
@@ -114,25 +85,44 @@ function start(){
 	//HOME PAGE
 
 	//HOME PAGE CONTAINER 1
-	$("#home-container-1").css({'height': 0.85*$(window).height() });
-	$("#home-container-1 > .text-container >  hr").css({'width': $("#home-container-1 > .text-container > span").width() });
+	$("#home-container-1").css({
+		'height': 0.85*$(window).height() 
+	});
+
+	$("#home-container-1 > .text-container >  hr").css({
+		'width': $("#home-container-1 > .text-container > span").width() 
+	});
 
 	//HOME PAGE CONTAINER 2
-	$("#home-container-2").css({'height': 0.5*$(window).height() });
-	$("#home-container-2 > .text-container").css({'height': ($("#home-container-2 > .text-container > p").height()) });
+	$("#home-container-2").css({
+		'height': 0.5*$(window).height() 
+	});
+
+	$("#home-container-2 > .text-container").css({
+		'height': ($("#home-container-2 > .text-container > p").height()) 
+	});
 
 	//HOMEPAGE CONTAINER 3
-	$("#home-container-3").css({'height': 0.85*$(window).height() });
-	$("#home-container-3 > .text-container").css({'height': ($("#home-container-3 > .text-container > p").height() +
-																	$("#home-container-3 > .text-container > h1").height() +
-																	  $("#home-container-3 > .text-container > a").height() + 40 )});
+	$("#home-container-3").css({
+		'height': 0.85*$(window).height() 
+	});
+	$("#home-container-3 > .text-container").css({
+		'height': ($("#home-container-3 > .text-container > p").height() +
+							 $("#home-container-3 > .text-container > h1").height() +
+							 $("#home-container-3 > .text-container > a").height() + 40)
+	});
 
-	if($("#home-container-3 > .text-container").height() > $("#home-container-3").height()){
-		$("#home-container-3").css({'height' : $("#home-container-3 > .text-container").height() + 20 });
+	if($("#home-container-3 > .text-container").height() 
+		> $("#home-container-3").height()) {
+		$("#home-container-3").css({
+			'height' : $("#home-container-3 > .text-container").height() + 20 
+		});
 	}
 
 	//HOME PAGE CONTAINER 5
-	$("#home-container-5 > .row > .small-12.large-6.columns").css({'height': 0.65*$(window).height() });
+	$("#home-container-5 > .row > .small-12.large-6.columns").css({
+		'height': 0.65*$(window).height() 
+	});
 
 	//FIRST YEAR ADVICE
 
@@ -140,7 +130,7 @@ function start(){
 	$("#fya-container-1").css({'height': 0.85*$(window).height() });
 
 	//FIRST YEAR ADVICE CONTAINER 2
-	$('#budgeting').hide();
+	/*$('#budgeting').hide();
 	$('#time-management').hide();
 	$('#work-experience').hide();
 
@@ -153,7 +143,7 @@ function start(){
 	$('#digital-systems').hide();
 	$('#discrete').hide();
 	$('#chemistry').hide();
-
+*/
 
 	$('.budgeting-button').click( function(){
 		$('#time-management').hide();
