f8de768732c4ff2e0f5284282e22efbd77c752b5
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2015-03-12 14:04:15 -0400

    Fixed schema to have blogs and users

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,18 @@ ActiveRecord::Schema.define(version: 20150212164524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "blog_posts", force: :cascade do |t|
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.string   "title",                        null: false
+    t.boolean  "is_published", default: false
+    t.date     "published_on"
+    t.integer  "user_id"
+    t.text     "body_field"
+  end
+
+  add_index "blog_posts", ["user_id"], name: "index_blog_posts_on_user_id", using: :btree
+
   create_table "club_members", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
@@ -43,5 +55,24 @@ ActiveRecord::Schema.define(version: 20150212164524) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "username",                            null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
