e5d7c1859f15ffb9868a5e50c8b50b8b037b4726
mtuck063 <meneliktucker@gmail.com>
2016-02-20 12:03:17 -0500

    Added new sesa logo

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,40 +1,11 @@
 $(document).on('page:change', function(){
 
-  // HOME PAGE CONTAINER 1
-
-  $("#home-container-1 > .text-container").css({ top: "35%", opacity: 1 });
-
   // JOIN
   
   $("span#close_successful_notice").click( function() { 
     $("#successful_notice").css({ "display" : "none" });
     return false;
   });
-
-  $("#home-container-1 > .text-container >  hr").css({
-    'width': $("#home-container-1 > .text-container > span").width() 
-  });
-
-  //HOME PAGE CONTAINER 2
-  $("#home-container-2 > .text-container").css({
-    'height': ($("#home-container-2 > .text-container > p").height()) 
-  });
-
-  //HOMEPAGE CONTAINER 3
-  $("#home-container-3 > .text-container").css({
-    'height': ($("#home-container-3 > .text-container > p").height() +
-               $("#home-container-3 > .text-container > h1").height() +
-               $("#home-container-3 > .text-container > a").height())
-  });
-
-  if($("#home-container-3 > .text-container").height() 
-    > $("#home-container-3").height()) {
-    $("#home-container-3").css({
-      'height' : $("#home-container-3 > .text-container").height() + 20 
-    });
-  }
-
-  //FIRST YEAR ADVICE
 
   //FIRST YEAR ADVICE CONTAINER 2
   $(".general-advice, .course-advice").hide();
