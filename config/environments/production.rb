48180757676247e89f4a9847d9fa4e7a60c320b6
Alex Kluew <dropbyalex@gmail.com>
2016-11-19 19:24:40 -0500

    Putting settings back to where it was. Heroku and Gmail still not happy.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,21 +71,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "uottawasesa_#{Rails.env}"
   # config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'uottawa-sesa.herokuapp.com' }
-  
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  #SMTP GMail Settings
+  config.action_mailer.default_url_options = { :host => 'uottawa-sesa.herokuapp.com'}
 
+  config.action_mailer.delivery_method = :smtp
+
+  #GMAIL SETUP
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV["SMTP_USER"],
-    password:             ENV["SMTP_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['SMTP_USER'],
+    :password => ENV['SMTP_PASSWORD'],
+    :authentication => 'plain',
+    :enable_starttls_auto => true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
