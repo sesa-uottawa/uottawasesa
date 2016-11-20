045eeb9b89a376dea5897221f22bf34cc7f943c7
Alex Kluew <dropbyalex@gmail.com>
2016-11-19 19:04:43 -0500

    Woops. I want to see Mailer errors. Not ignore them.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,11 +74,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'uottawa-sesa.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     domain: "uottawa-sesa.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
